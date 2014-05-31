22af4c34e116eccbde651b6c79cd9585f6e3956b
Fahad Mukunzi <fahadm507@gmail.com>
2014-05-31 16:31:40 -0400

    inserted comments and added more some more functionality

--- server.rb
+++ server.rb
@@ -2,22 +2,6 @@ require 'sinatra'
 require 'pry'
 require 'pg'
 
-
-
-
-
-
-
-=begin
-  Visiting /recipes/:id will show the details for a recipe with the given ID.
-  The page must include the recipe name, description, and instructions.
-  The page must list the ingredients required for the recipe.
-  Setup
-=end
-
-#building a sinatra application that allows users to view a list of recipes
-#and make a choice of the ones that seems appetizing.
-
 #1. Connecting to the database
 def db_connection
   begin
@@ -30,13 +14,14 @@ end
 
 #2.Getting a list of all the recipes in the databse, sorted alphabetically.
 def get_recipes
-  query = 'SELECT recipes.name, recipes.id FROM recipes'
+  query = 'SELECT recipes.name, recipes.id FROM recipes LIMIT 20 OFFSET #{offset}'
   results= db_connection do |conn|
     conn.exec(query)
   end
   @results = results.sort_by {|result| result['name']}
 end
 
+#Getting the recipe details from the database
 def get_recipes_details(params)
   query = 'SELECT recipes.name, recipes.id, recipes.description,
   recipes.instructions,ingredients.name AS ingredient_name
@@ -55,15 +40,20 @@ end
 
 #4.This get method gets the recipes and render index page to dipaly them.
 get '/recipes' do
+  #paginating the list of recipes
+  @page = params[:page] || 1
+  @page = @page.to_i
+  offset = (@page-1) * 20
+
   get_recipes
 
   erb :index
 end
 
-#4.A method that gets information about the recipes and render show page
+#4.get method that gets information about the recipes and render show page
 #to dipaly the output
 get '/recipes/:id' do
-  get_recipes_details(params)
+  get_recipes_details(params)#returns @recipes_details
 
   erb :'show'
 end
